e5e93e3bb9cf7f36dae9ad797dac0fc99314567f
Jakwes <jakwesoneilgonzalez@gmail.com>
2024-04-08 13:14:52 +0800

    Polished Navbar
    Added the font, polished UI

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,57 @@ const App: React.FC = () => {
             </>
           }
         />
+        <Task
+          title="Task 1"
+          content={
+            <>
+              <p>Introduction</p>
+              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+              <p><b>Learning Objectives:</b></p>
+              <ul>
+                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+                <li>Understanding the various phases of the UKC</li>
+                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+              </ul>
+            </>
+          }
+        />
+        <Task
+          title="Task 1"
+          content={
+            <>
+              <p>Introduction</p>
+              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+              <p><b>Learning Objectives:</b></p>
+              <ul>
+                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+                <li>Understanding the various phases of the UKC</li>
+                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+              </ul>
+            </>
+          }
+        />
+        <Task
+          title="Task 1"
+          content={
+            <>
+              <p>Introduction</p>
+              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+              <p><b>Learning Objectives:</b></p>
+              <ul>
+                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+                <li>Understanding the various phases of the UKC</li>
+                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+              </ul>
+            </>
+          }
+        />
         
       </div>
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,15 +4,18 @@ import './NavBar.css'; // Import CSS for Navbar styling
 import logo from "/src/assets/BeGuiVir.png";
 const NavBar = () => {
   return (
+    
     <nav className="navbar">
       <div className="navbar-logo">
+        
         <img src={logo} alt="logo"/>
+        <p>beyondguidancevirtual</p>
       </div>
       <ul className="navbar-links">
         <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">About Us</a></li>
+
       </ul>
     </nav>
   );
