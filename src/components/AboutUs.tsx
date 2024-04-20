a324ddc93e1368af44dbb19b0a330356cf15f17a
jakwesoneil <jakwesoneilgonzalez@gmail.com>
2024-04-20 22:08:11 +0800

    Fixed Home Page

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -4,11 +4,21 @@ import './AboutUs.css'; // Import your CSS file
 const jakwes_barong = '/src/assets/jakwes_barong.jpg';
 const reanne = '/src/assets/reanne.jpg';
 const miguel = '/src/assets/miguel.jpg';
+const typescript = '/src/assets/typescript.png';
+const reactjs ='src/assets/react.png';
+const php = '/src/assets/php.png';
+const unity = '/src/assets/unity.png';
 
 const developers = [
   { id: 1, name: 'Jacques Oneil Gonzalez', school: 'Mapua University', image: jakwes_barong },
   { id: 2, name: 'Reanne Angela Buccat', school: 'Mapua University', image: reanne },
   { id: 3, name: 'Miguel Antonio Villaruel', school: 'Mapua University', image: miguel }
+];
+const languagesAndTools = [
+  { id: 1, name: 'Typescript', image: typescript },
+  { id: 2, name: 'React JSX', image:reactjs },
+  { id: 3, name: 'PHP', image: php },
+  { id: 4, name: 'Unity', image: unity }
 ];
 
 const AboutUs: React.FC = () => {
@@ -29,9 +39,19 @@ const AboutUs: React.FC = () => {
         ))}
       </div>
       <h2>Languages and Tools Used</h2>
-      <p>Typescript, React JSX, PHP, Unity </p>
-      <h2>Motto</h2>
-      <p>BasicGearsVirtual aims to teach students about machine shop training that they could use to further improve their career goals</p>
+      <div className="tools-list">
+        {languagesAndTools.map(tool => (
+          <div className="tool" key={tool.id}>
+            <div className="tool-info">
+              <p className="name">{tool.name}</p>
+            </div>
+            <div className="tool-image-container">
+              <img className="tool-image" src={tool.image} alt={tool.name} />
+            </div>
+          </div>
+        ))}
+      </div>
+      
     </div>
   );
 };
