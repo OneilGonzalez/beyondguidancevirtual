c3d066d6e1c98ff4e5f107d760827f0c96978cd0
jakwesoneil <jakwesoneilgonzalez@gmail.com>
2024-04-21 14:13:04 +0800

    Working Check Answer Button and  Updated Home

--- src/pages/Topics.tsx
+++ src/pages/Topics.tsx
@@ -8,93 +8,78 @@ const Topics: React.FC = () => {
       <div className="container" > 
         <Task
           title="Task 1"
-          content={
-            <>
-              <p>Introduction</p>
-              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
-              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
-              <p><b>Learning Objectives:</b></p>
-              <ul>
-                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
-                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
-                <li>Understanding the various phases of the UKC</li>
-                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
-              </ul>
-            </>
-          }
-        />
+          content={<>
+            <p>Introduction</p>
+            <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+            <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+            <p><b>Learning Objectives:</b></p>
+            <ul>
+              <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+              <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+              <li>Understanding the various phases of the UKC</li>
+              <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+            </ul>
+          </>} correctAnswer={''}        />
         <br></br>
         <Task
           title="Task 2"
-          content={
-            <>
-              <p>Introduction</p>
-              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
-              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
-              <p><b>Learning Objectives:</b></p>
-              <ul>
-                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
-                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
-                <li>Understanding the various phases of the UKC</li>
-                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
-              </ul>
-            </>
-          }
-        />
+          content={<>
+            <p>Introduction</p>
+            <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+            <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+            <p><b>Learning Objectives:</b></p>
+            <ul>
+              <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+              <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+              <li>Understanding the various phases of the UKC</li>
+              <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+            </ul>
+          </>} correctAnswer={''}        />
         <br></br>
         <Task
           title="Task 3"
-          content={
-            <>
-              <p>Introduction</p>
-              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
-              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
-              <p><b>Learning Objectives:</b></p>
-              <ul>
-                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
-                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
-                <li>Understanding the various phases of the UKC</li>
-                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
-              </ul>
-            </>
-          }
-        />
+          content={<>
+            <p>Introduction</p>
+            <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+            <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+            <p><b>Learning Objectives:</b></p>
+            <ul>
+              <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+              <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+              <li>Understanding the various phases of the UKC</li>
+              <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+            </ul>
+          </>} correctAnswer={''}        />
         <br></br>
         <Task
           title="Task 4"
-          content={
-            <>
-              <p>Introduction</p>
-              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
-              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
-              <p><b>Learning Objectives:</b></p>
-              <ul>
-                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
-                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
-                <li>Understanding the various phases of the UKC</li>
-                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
-              </ul>
-            </>
-          }
-        />
+          content={<>
+            <p>Introduction</p>
+            <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+            <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+            <p><b>Learning Objectives:</b></p>
+            <ul>
+              <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+              <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+              <li>Understanding the various phases of the UKC</li>
+              <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+            </ul>
+          </>} correctAnswer={''}        />
         <br></br>
         <Task
           title="Task 5"
-          content={
-            <>
-              <p>Introduction</p>
-              <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
-              <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
-              <p><b>Learning Objectives:</b></p>
-              <ul>
-                <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
-                <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
-                <li>Understanding the various phases of the UKC</li>
-                <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
-              </ul>
-            </>
-          }
-        />
+          content={<>
+            <p>Introduction</p>
+            <p>Understanding the behaviours, objectives and methodologies of a cyber threat is a vital step to establishing a strong cybersecurity defence (known as a cybersecurity posture).</p>
+            <p>In this room, you will be introduced to the UKC (Unified Kill Chain) framework that is used to help understand how cyber attacks occur.</p>
+            <p><b>Learning Objectives:</b></p>
+            <ul>
+              <li>Understanding why frameworks such as the UKC are important and helpful in establishing a good cybersecurity posture</li>
+              <li>Using the UKC to understand an attacker's motivation, methodologies and tactics</li>
+              <li>Understanding the various phases of the UKC</li>
+              <li>Discover that the UKC is a framework that is used to complement other frameworks such as MITRE.</li>
+            </ul>
+          </>} correctAnswer={'hi'}        />
         
       </div>
     </div>
